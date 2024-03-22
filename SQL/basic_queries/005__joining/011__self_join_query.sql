55542af76541d77cdb8faa63933bc0e38c49b543
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-03-22 19:07:10 +0600

    Update 011__self_join_query.sql

--- SQL/basic_queries/005__joining/011__self_join_query.sql
+++ SQL/basic_queries/005__joining/011__self_join_query.sql
@@ -1,7 +1,3 @@
-SELECT 
-    e1.employee_name AS Employee,
-    e2.employee_name AS Manager
-FROM 
-    Employee e1, Employee e2
-WHERE 
-    e1.manager_id = e2.employee_id;
+SELECT e1.employee_name AS Employee, e2.employee_name AS Manager
+FROM Employee e1, Employee e2
+WHERE e1.manager_id = e2.employee_id;
