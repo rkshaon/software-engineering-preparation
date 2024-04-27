b9f09de68077e2c887235b4752f4010633a289c4
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-04-27 19:32:52 +0600

    Python - data types - tuple - traversing

--- languages/Python/001__basics/002__data_types/002__tuple/007__traversing_tuple.py
+++ languages/Python/001__basics/002__data_types/002__tuple/007__traversing_tuple.py
@@ -1,4 +1,4 @@
-# Traverse nested tuple using loop
+
 monthly_incomes = (
     ("January", 5000),
     ("February", 5500),
@@ -14,9 +14,33 @@ monthly_incomes = (
     ("December", 5500)
 )
 
+# Traverse nested tuple using loop
 total_income = 0
 
 for income in monthly_incomes:
     total_income += income[1]
 
 print(total_income)
+
+
+# Traverse nested tuple using loop and tuple unpacking
+quarter_income = 0
+
+for index, (month, income) in enumerate(monthly_incomes, start=1):
+    print(f"{month:>10}: {income}")
+    quarter_income += income
+    if index % 3 == 0:
+        print("-" * 20)
+        print(f"{'Quarter':>10}: {quarter_income}", end="\n\n")
+        quarter_income = 0
+
+numbers_in_str = ("2", "9", "5", "1", "6")
+
+# Traverse using list comprehension
+numbers1 = tuple([int(number) for number in numbers_in_str])    # (2, 9, 5, 1, 6)
+
+# Traverse using generator expressoin
+numbers2 = tuple(int(number) for number in numbers_in_str)      # (2, 9, 5, 1, 6)
+
+
+
