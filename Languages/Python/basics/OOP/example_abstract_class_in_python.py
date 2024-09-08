3a7261cfc2890c260dc8877db64e7cdacb837dbd
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2024-09-08 13:16:55 +0600

    Python - OOP - abstract class added

---
+++ Languages/Python/basics/OOP/example_abstract_class_in_python.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
